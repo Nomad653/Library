c42d90fd8efa7ec4897a6c0fb4e2d7fcba5c0b7f
Nurlann11 <nurlan.ekberov.1010@mail.ru>
2023-10-03 20:44:29 +0400

    cccc

--- scraping_1.py
+++ scraping_1.py
@@ -3,11 +3,18 @@ import json
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-service = Service('.\chromedriver.exe')
-driver = webdriver.Chrome(service=service)
+x = Service('.\chromedriver.exe')
+driver = webdriver.Chrome(service=x)
 driver.maximize_window()
 driver.get('https://www.kitapyurdu.com/')
+
 # edebiyyat ,adi, isbn, yayin tarixi,muellif
-edebiyyat=driver.find_element(By.XPATH, "//span[@class='mn-strong common-sprite' and text()='Çok Satan Kitaplar' ]")
+
+edebiyyat=driver.find_element(By.XPATH, "//span[@class='mn-strong common-sprite' and text()='Çok Satan Kitaplar']").click()
+time.sleep(1)
+cok_satilan=driver.find_element(By.XPATH,"//a[@class='mn-icon icon-angleRight']//strong[text()='Edebiyat']").click()
+accept=driver.find_element(By.ID,'js-popup-accept-button').click()
+
 time.sleep(3)
+
 driver.quit()
