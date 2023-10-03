c0906a72de2a4600f86fa007511f3de6b448a0dd
Khayala <fatullaeva@gmail.com>
2023-10-03 19:53:24 +0400

    kitab adinda class yaradildi

---
+++ kitabxana.py
@@ -0,0 +1,10 @@
+
+class kitab():
+ __init__(self,id,ad,janr,muellif,buraxilish_ili,espn_adres):
+self.id = id
+self.ad = ad 
+self.janr = janr
+self.muellif = muellif
+self.buraxilish_ili = buraxilish_ili
+self.espn_adres = espn.adres 
+ 
